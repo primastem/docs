aed44044360984872a1b318b413ea035d90695d6
PrimaSTEM <158175504+primastem@users.noreply.github.com>
2025-04-30 16:30:51 +0200

    fix link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/primastem/docs/tree/main/docs/',
+            'https://github.com/primastem/docs/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -59,8 +59,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/primastem/docs/tree/main/docs/',
-
+            'https://github.com/primastem/docs/tree/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
