a87845ef0e6de4652b2efe128d39a76938b3e89b
PrimaSTEM <158175504+primastem@users.noreply.github.com>
2025-05-01 13:11:58 +0200

    buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/fr/docs/intro">
+            to="/fr/">
             Français
           </Link>
         </div>
