6d3867218103e44b72340b6ef032d5dd8401b6c7
PrimaSTEM <158175504+primastem@users.noreply.github.com>
2025-05-02 12:43:15 +0200

    upd social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,7 +84,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/primastem-social-card.jpg',
       navbar: {
         title: 'docs.PrimaSTEM.com',
         logo: {
