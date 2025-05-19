a1b820361ddb7599956a67ac03ad60470f978136
PrimaSTEM <158175504+primastem@users.noreply.github.com>
2025-05-19 16:01:03 +0200

    upd background

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -214,6 +214,7 @@ const config = {
                },
       prism: {
         theme: prismThemes.github,
+        // darkTheme: prismThemes.dracula,
         darkTheme: prismThemes.dracula,
               },      
     }),
